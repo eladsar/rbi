c11fb2aab31b36ea23a7e50ad1a37037eb5f21be
rothariel <roth.ariel.phil@gmail.com>
2019-02-10 13:20:58 +0200

    Update run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -5,5 +5,4 @@
 # nvidia-docker run --rm -it ${NET} ${IPC} ${VOLUMES} ${CONTNAME} ${IMAGENAME} bash
 #docker container run --rm --name ${CONTNAME} ${IMAGENAME} 
 #--shm-size=<requested memory size>
-docker container run --rm -it --net=host --ipc=host --name rbi1 local:rbi bash
-docker container run --rm -it --net=host --ipc=host --name test pytorch/pytorch:latest bash
+docker container run --rm -it --net=host --ipc=host --name rbi1 local:rbi bas
