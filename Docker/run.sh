838e3f763fcc4d997cdc22b3a261093c5ff8581f
rothariel <roth.ariel.phil@gmail.com>
2019-02-10 15:57:45 +0200

    Update run.sh

--- Docker/run.sh
+++ Docker/run.sh
@@ -5,4 +5,6 @@
 # nvidia-docker run --rm -it ${NET} ${IPC} ${VOLUMES} ${CONTNAME} ${IMAGENAME} bash
 #docker container run --rm --name ${CONTNAME} ${IMAGENAME} 
 #--shm-size=<requested memory size>
-docker container run --rm -it --net=host --ipc=host --name rbi1 local:rbi bas
+docker container run --rm -it --net=host --ipc=host --name rbi1 local:rbi bash
+
+nvidia-docker run --rm -it --net=host --ipc=host --name rbi1 local:rbi bash
